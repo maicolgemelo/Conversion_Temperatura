4988b1b4fd69f551b53a8c3b748e050b59ce363c
Michael Escobar Galeano <maicolgemelo@gmail.com>
2019-09-22 11:50:59 -0500

    Ejercicio el cual convierte los grados Celsius a Fahrenheit

---
+++ Conversion_Temperatura/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Conversion_Temperatura
+{
+    public partial class Form1 : Form
+    {
+        Temperaturas temperaturas = new Temperaturas();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvertir_Click(object sender, EventArgs e)
+        {
+            temperaturas.setGCelsius(double.Parse(txtGcelsius.Text));
+            lblResultado.Text = " Resultado: " + temperaturas.Convertir().ToString();
+            lblResultado.Visible = true;
+            txtGcelsius.Text = "";
+            
+        }
+    }
+}
